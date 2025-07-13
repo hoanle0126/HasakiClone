6ff9fcc97afebaedd0517cd0ee7f09594c802555
hoanle0126 <hoanle0126@gmail.com>
2025-07-13 14:48:34 +0700

    Update crawler to playwight

--- crawler/index.py
+++ crawler/index.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
 import json
 
@@ -8,21 +8,26 @@ with open("new_brands.json", 'r', encoding="utf-8") as f:
     brands = json.load(f)
 
 
-def getElements(url):
-    driver = webdriver.Chrome()
-    driver.get(url)
+def getElements(playwright, url):
+    browser = playwright.chromium.launch()
+    page = browser.new_page()
+    page.goto(url)
     
-    html = driver.page_source
+    html = page.content()
     soup = BeautifulSoup(html, "html.parser")
     elements = {
-    "name":soup.select_one(".bg-white .m-auto.container .flex.gap-4.p-5.pb-0.bg-white .w-full.text-sm h1").text,
-    "description":soup.select_one(".bg-white .m-auto.container .flex.gap-4.p-5.pb-0.bg-white .w-full.text-sm div").text
+        "name": soup.select_one(".bg-white .m-auto.container .flex.gap-4.p-5.pb-0.bg-white .w-full.text-sm h1").text.strip() if soup.select_one(".bg-white .m-auto.container .flex.gap-4.p-5.pb-0.bg-white .w-full.text-sm h1") else "",
+        "description": soup.select_one(".bg-white .m-auto.container .flex.gap-4.p-5.pb-0.bg-white .w-full.text-sm div").text.strip() if soup.select_one(".bg-white .m-auto.container .flex.gap-4.p-5.pb-0.bg-white .w-full.text-sm div") else ""
     }
+    browser.close()
     return elements
 
 
-for brand in brands:
-    list_brands.append(getElements(brand))
+with sync_playwright() as playwright:
+    for brand in brands:
+        elements = getElements(playwright, brand)
+        list_brands.append(elements)
+        print(f"Đã thêm: {elements['name']}")
 
-with open("list_brands", 'w', encoding="utf-8") as file:
-    json.dump(list_brands, ensure_ascii=False, indent=4)
+with open("list_brands.json", 'w', encoding="utf-8") as file:
+    json.dump(list_brands, file, ensure_ascii=False, indent=4)
