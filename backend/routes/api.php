3800a95f194a08d983caa05828859bc954fe98f0
hoanle0126@gmail.com <hoanle0126@gmail.com>
2025-11-28 20:35:49 +0700

    Fix lại ci cd

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -72,7 +72,7 @@ Route::get('/debug-config', function () {
 
 
 
-Route::get('/test-cicd', function () {
+Route::get('/test-ci-cd', function () {
     return [
         'message' => 'Kiểm tra cấu hình Database thực tế',
         'db_host_from_config' => config('database.connections.mysql.host'),
