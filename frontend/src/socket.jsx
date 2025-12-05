d47335a9b5fbece4657f407c4f252aaa90147d52
hoanle0126@gmail.com <hoanle0126@gmail.com>
2025-12-06 00:44:27 +0700

    THem socket

--- frontend/src/socket.jsx
+++ frontend/src/socket.jsx
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001", {
+const socket = io(`${import.meta.env.VITE_SOCKET_URL}`, {
     transports: ["websocket"],
 });
 
