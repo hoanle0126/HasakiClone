b0fcec54b0e75d8b196e319ee709c977d4b53450
hoanle0126@gmail.com <hoanle0126@gmail.com>
2025-12-06 01:20:36 +0700

    asd

--- socket/server.js
+++ socket/server.js
@@ -30,7 +30,7 @@ io.on("connection", (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
     console.log(`Socket.io server running on port ${PORT}`);
