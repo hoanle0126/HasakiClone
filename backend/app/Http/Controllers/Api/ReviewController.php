6395a106c8edda34a4634bdc160b408b3d192bd3
hoanle0126@gmail.com <hoanle0126@gmail.com>
2025-12-06 07:06:29 +0700

    Hoàn thiện phần auto comment

--- backend/app/Http/Controllers/Api/ReviewController.php
+++ backend/app/Http/Controllers/Api/ReviewController.php
@@ -93,7 +93,7 @@ class ReviewController extends Controller
                     'product_id' => $request->product_id,
                     'data' => [
                         'message' => $request->reply_content,
-                        "reviews" => Review::where("product_id",$request->product_id)
+                        "reviews" => Product::where("id", $request->product_id)->first()["reviews"]
                     ]
                 ]
             ]);
